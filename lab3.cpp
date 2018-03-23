ea221861737514b0a250f7ebddadf13eda924552
AntonAgalakov <agalakovant98@gmail.com>
2018-03-23 18:52:33 +0700

    final

--- lab3.cpp
+++ lab3.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
 #include <cmath>
+#include "lab3.h"
 #define EPS 1E-6
 
 using namespace std;
 double l[2] = {0.0, 5.0};
-double equation(double x)
-{
-	return sqrt(abs(cos(x))) - x;
-}
-
 double halfvision(double x, double x2, double (*f)(double))
 {
 	double temp = (x+x2)/2;
-	while(abs((*f)(temp)) > EPS)
+	while(abs(x2-x) > EPS)
 	{
 		if((*f)(temp) < 0 && (*f)(x) > 0)
 			x2 = temp;
@@ -28,17 +24,28 @@ double halfvision(double x, double x2, double (*f)(double))
 }
 double chords(double x, double x2, double (*f)(double))
 {
-	while(abs(x2-x) >EPS)
+	while(abs(x2-x) > EPS)
 	{
 		x = x2 -(x2-x)*f(x2)/(f(x2) - f(x));
 		x2 = x +(x-x2)*f(x) / (f(x) - f(x2));
 	}
 	return x2;
 }
+double Nuton(double x, double x2, double (*f)(double), double (*df)(double))
+{
+	while(abs(x2-x) > EPS)
+	{
+		x = x2;
+		x2 = x -(f(x)/df(x));
+	}
+	return x2;
+}
 int main()
 {
-	double (*f)(double) = &equation;
+	double (*f)(double) = &equation2;
+	double (*df)(double) = &dequation2;
 	cout << "The root of the equation = " << halfvision(l[0], l[1], f) << endl;
 	cout << "The root of the equation = " << chords(l[0], l[1], f) << endl;
+	cout << "The root of the equation = " << Nuton(l[0], l[1], f, df) << endl;
 	return 0;
 }
---
+++ lab3.h
@@ -0,0 +1,18 @@
+double equation(double x)
+{
+	return pow(x, 2.0) + x - 5.0;
+}
+double dequation(double x)
+{
+	return 2.0*x+1;
+}
+double equation2(double x)
+{
+	return pow(cos(x / 4.0), 2.0) - x;
+}
+double dequation2(double x)
+{
+	return -0.25 * sin(x / 4.0) - 1.0;
+}
+
+
